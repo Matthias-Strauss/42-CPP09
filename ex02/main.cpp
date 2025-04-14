d04f3b404bfc81f1b64dc86d3caddeb6f4feaac5
Matthias Strauss <mstrauss@student.42heilbronn.de>
2025-04-14 17:10:37 +0200

    ex03: parsing

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -5,14 +5,15 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mstrauss <mstrauss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/14 15:00:54 by mstrauss          #+#    #+#             */
-/*   Updated: 2025/04/14 15:50:20 by mstrauss         ###   ########.fr       */
+/*   Created: 2025/04/14 15:00:54 by mstrauss          #+#             */
+/*   Updated: 2025/04/14 16:51:18 by mstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
-#include <array>
+
 #include <ctime>
+#include <deque>
 #include <iostream>
 #include <vector>
 
@@ -23,6 +24,33 @@ double getUnixTime(void) {
     return 0;
 
   return (tv.tv_sec + (tv.tv_nsec / 1000000000.0));
+}
+
+int parseNumbers(int ac, char **av, std::vector<int> *vec,
+                 std::deque<int> *deq) {
+
+  try {
+    for (int i = 1; i < ac; ++i) {
+      char *endptr;
+      long num = std::strtol(av[i], &endptr, 10);
+
+      if (*endptr != '\0')
+        throw std::invalid_argument(
+            "Error: Invalid input - Non-numeric character.");
+      if (num < 0)
+        throw std::invalid_argument("Error: Invalid input - Negative number.");
+      if (num > std::numeric_limits<int>::max()) {
+        throw std::invalid_argument("Error: Invalid input - Number out of int "
+                                    "range.");
+      }
+      vec->push_back(static_cast<int>(num));
+      deq->push_back(static_cast<int>(num));
+    }
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << '\n';
+    return 1;
+  }
+  return 0;
 }
 
 int main(int ac, char **av) {
@@ -37,7 +65,9 @@ int main(int ac, char **av) {
   double time_diff;
 
   std::vector<int> vector;
-  std::array<int, 10> array; // Allocate later with dynamic size
+  std::deque<int> deque;
+  if (parseNumbers(ac, av, &vector, &deque))
+    return 1;
 
   try {
     start_time = getUnixTime();
@@ -45,14 +75,14 @@ int main(int ac, char **av) {
     stop_time = getUnixTime();
     time_diff = start_time - stop_time;
     std::cout << "Time to process a range of " << ac - 1 << " elements with "
-              << Container << " : " << time_diff << "ms" << std::endl;
+              << ContainerName << " : " << time_diff << "ms" << std::endl;
 
     start_time = getUnixTime();
-    fordJohnsonAlgo(vector);
+    fordJohnsonAlgo(deque);
     stop_time = getUnixTime();
     time_diff = start_time - stop_time;
     std::cout << "Time to process a range of " << ac - 1 << " elements with "
-              << Container << " : " << time_diff << "ms" << std::endl;
+              << ContainerName << " : " << time_diff << "ms" << std::endl;
 
   } catch (const std::exception &e) {
     std::cerr << e.what() << '\n';
