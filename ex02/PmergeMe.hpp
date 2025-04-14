500cfe97d0f748d615b9c8245e9e2f66c3bfbb19
Matthias Strauss <mstrauss@student.42heilbronn.de>
2025-04-14 18:41:08 +0200

    ex03: added orthodox canonical requirements. quick push to switch machines, this mac is tweaking

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,12 +6,25 @@
 /*   By: mstrauss <mstrauss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 15:02:00 by mstrauss          #+#    #+#             */
-/*   Updated: 2025/04/14 15:03:55 by mstrauss         ###   ########.fr       */
+/*   Updated: 2025/04/14 18:40:13 by mstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-typedef Class XXX {
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+#include <algorithm>
+#include <deque>
+#include <vector>
 
+class PMergeMe {
 public:
+  PMergeMe();
+  PMergeMe(const PMergeMe &other);
+  PMergeMe &operator=(const PMergeMe &other);
+  ~PMergeMe();
+
 private:
-}
+  static int comparisons = 0;
+};
+
+#endif PMERGEME_HPP
